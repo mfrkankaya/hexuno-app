807b028097b5edfeffa8ad1059ddb1370fb4a13d
Furkan Kaya <mfrkankaya@gmail.com>
2023-12-03 14:36:12 +0300

    Refactor sign-in page with react-query mutation for better scalability and maintainability
    This commit refactors the sign-in page by introducing react-query's
    useMutation hook to handle sign-in functionality. By leveraging this
    hook, the code is now more modular and follows the best practices for
    managing asynchronous actions. The new approach simplifies the logic and
    improves the scalability and maintainability of the codebase.
    Additionally, the sign-in buttons now have disabled states when a
    sign-in request is in progress. This update enhances the user experience
    by preventing multiple sign-in requests and providing visual feedback
    during the sign-in process.
    
    Resolves #123

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import { TouchableOpacity, View } from "react-native"
 import { GoogleSignin } from "@react-native-google-signin/google-signin"
+import { useMutation } from "@tanstack/react-query"
 import { router } from "expo-router"
 import {
   GoogleAuthProvider,
@@ -10,10 +11,36 @@ import {
 } from "firebase/auth"
 
 import { auth } from "@/lib/firebase"
+import { cn } from "@/lib/utils"
 import { Text } from "@/components/ui/text"
 import Logo from "@/svg/Logo.svg"
 
 export default function AuthPage() {
+  const googleSignInMutation = useMutation({
+    mutationFn: async () => {
+      GoogleSignin.configure({
+        webClientId:
+          "863791832999-6950mla0i12ocop4fu2l9d8o1udks5hb.apps.googleusercontent.com",
+      })
+
+      await GoogleSignin.hasPlayServices()
+      const userInfo = await GoogleSignin.signIn()
+      await signInWithCredential(
+        auth,
+        GoogleAuthProvider.credential(userInfo.idToken)
+      )
+    },
+  })
+
+  const anonymousSignInMutation = useMutation({
+    mutationFn: async () => {
+      await signInAnonymously(auth)
+    },
+  })
+
+  const isPending =
+    googleSignInMutation.isPending || anonymousSignInMutation.isPending
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -24,44 +51,14 @@ export default function AuthPage() {
     return () => unsubscribe()
   })
 
-  async function handleGoogleSignIn() {
-    GoogleSignin.configure({
-      webClientId:
-        "863791832999-6950mla0i12ocop4fu2l9d8o1udks5hb.apps.googleusercontent.com",
-    })
-
-    try {
-      await GoogleSignin.hasPlayServices()
-      const userInfo = await GoogleSignin.signIn()
-      await signInWithCredential(
-        auth,
-        GoogleAuthProvider.credential(userInfo.idToken)
-      )
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  async function handleAnonymousSignIn() {
-    await signInAnonymously(auth)
-  }
-
   return (
     <View className="flex-1 justify-center items-center">
       <View className="px-6 w-full">
         <View className="hidden dark:flex mx-auto mb-4">
-        <Logo
-            width={128}
-            height={128}
-            className="text-white"
-          />
+          <Logo width={128} height={128} className="text-white" />
         </View>
         <View className="dark:hidden mx-auto mb-4">
-          <Logo
-            width={128}
-            height={128}
-            className="text-zinc-950"
-          />
+          <Logo width={128} height={128} className="text-zinc-950" />
         </View>
         <Text className="text-center font-lato-black text-4xl leading-none">
           HEXUNO
@@ -72,8 +69,14 @@ export default function AuthPage() {
         </Text>
 
         <TouchableOpacity
-          onPress={handleGoogleSignIn}
-          className="w-full items-center justify-center mt-8 h-16 rounded-2xl bg-zinc-950 dark:bg-white"
+          onPress={() => googleSignInMutation.mutate()}
+          disabled={isPending}
+          className={cn(
+            "w-full items-center justify-center mt-8 h-16 rounded-2xl bg-zinc-950 dark:bg-white",
+            {
+              "opacity-50": isPending,
+            }
+          )}
         >
           <Text className="text-white font-bold dark:text-zinc-950">
             Sign in with Google
@@ -81,8 +84,11 @@ export default function AuthPage() {
         </TouchableOpacity>
 
         <TouchableOpacity
-          onPress={handleAnonymousSignIn}
-          className="mt-2 w-fit mx-auto p-2"
+          onPress={() => anonymousSignInMutation.mutate()}
+          disabled={isPending}
+          className={cn("mt-2 w-fit mx-auto p-2", {
+            "opacity-50": isPending,
+          })}
         >
           <Text>Continue anonymously</Text>
         </TouchableOpacity>
